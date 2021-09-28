513ba73d3e4db40ed50e1e94655ba3f865c11fc0
tmgerard <tom.gerard@gmail.com>
2021-09-28 05:22:17 -0500

    Added Point2D and DoubleCompare.

---
+++ Mechanics/Geometry/DoubleCompare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechanics.Geometry
+{
+    public static class DoubleCompare
+    {
+        public const double CompareTolerance = 1e-10;
+        public static bool AreEqual(double a, double b, double tolerance = CompareTolerance)
+        {
+            return Math.Abs(a - b) < tolerance;
+        }
+
+        public static bool IsEffectivelyZero(double a, double tolerance = CompareTolerance)
+        {
+            return AreEqual(a, 0.0, tolerance);
+        }
+    }
+}
