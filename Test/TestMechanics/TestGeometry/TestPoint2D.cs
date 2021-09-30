7e82e87180926d4ba5943990ba8c27d27b147e96
tmgerard <tom.gerard@gmail.com>
2021-09-29 21:44:39 -0500

    Added Point2D tests

---
+++ Test/TestMechanics/TestGeometry/TestPoint2D.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mechanics.Geometry;
+
+namespace TestMechanics.TestGeometry
+{
+    [TestClass]
+    public class TestPoint2D
+    {
+        Point2D pointOne = new Point2D(1, 2);
+        Point2D pointTwo = new Point2D(2, 4);
+        Vector2D displaceVec = new Vector2D(1, 1);
+
+        [TestMethod]
+        public void TestDisplacedBy()
+        {
+            Point2D expected = new Point2D(2, 3);
+            Point2D actual = pointOne.DisplacedBy(displaceVec, 1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDistanceTo()
+        {
+            double expected = 2.2361;
+            double actual = pointOne.DistanceTo(pointTwo);
+            Assert.AreEqual(expected, actual, 0.0001);
+        }
+
+        [TestMethod]
+        public void TestMinusOperator()
+        {
+            Vector2D expected = new Vector2D(1, 2);
+            Vector2D actual = pointTwo - pointOne;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
