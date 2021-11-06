468ce377451b6c4b2004af377478bb28d8832177
tmgerard <tom.gerard@gmail.com>
2021-11-05 20:32:36 -0500

    Added Polygon class

---
+++ Mechanics/Geometry/Shapes/Polygon.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechanics.Geometry.Shapes
+{
+    public class Polygon
+    {
+        private List<Point2D> vertices;
+
+        public Polygon(List<Point2D> vertices)
+        {
+            this.Vertices = vertices;
+        }
+
+        public List<Point2D> Vertices
+        {
+            get => Vertices;
+            set
+            {
+                if (value.Count < 3)
+                {
+                    throw new ArgumentOutOfRangeException("Three or more vertices required for polygon.");
+                }
+                vertices = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns area of <see cref="Polygon"/>.
+        /// </summary>
+        /// <remarks>Assumes non-intersecting polygon edges.</remarks>
+        /// <returns>Area of <see cref="Polygon"/></returns>
+        public double Area()
+        {
+            double sum = 0;
+            int next = 0;
+            for (int i = 0; i < vertices.Count - 1; i++)
+            {
+                next = GetNextIndex(i);
+
+                sum += (vertices[i].x * vertices[next].y) - (vertices[next].x * vertices[i].y);
+            }
+
+            return Math.Abs(0.5 * sum);
+        }
+
+        /// <summary>
+        /// Returns <see cref="Point2D"/> representing the centroid of <see cref="Polygon"/> area
+        /// </summary>
+        /// <returns>Polygon centroid</returns>
+        public Point2D Centroid()
+        {
+            double Cx = 0;
+            double Cy = 0;
+            
+            int next = 0;
+            for (int i = 0; i < vertices.Count - 1; i++)
+            {
+                next = GetNextIndex(i);
+
+                Cx += (vertices[i].x + vertices[next].x) * (vertices[i].x * vertices[next].y - vertices[next].x * vertices[i].y);
+                Cy += (vertices[i].y + vertices[next].y) * (vertices[i].x * vertices[next].y - vertices[next].x * vertices[i].y);
+            }
+
+            double divisor = 6.0 * Area();
+            return new Point2D(Cx / divisor, Cy / divisor);
+        }
+
+        /// <summary>
+        /// Returns the next index for area and centroid calculations, accounting for wrap-around
+        /// if the initial vertex is not repeated at the end of the list to close out the polygon.
+        /// </summary>
+        /// <param name="i">List index</param>
+        /// <returns>Next list index</returns>
+        private int GetNextIndex(int i)
+        {
+            int next;
+            if (i == vertices.Count - 1 && vertices[0] != vertices[vertices.Count - 1])
+            {
+                next = 0;
+            }
+            else
+            {
+                next = i + 1;
+            }
+
+            return next;
+        }
+    }
+}
---
+++ Test/TestMechanics/Geometry/Shapes/TestPolygon.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mechanics.Geometry.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mechanics.Geometry;
+
+namespace Mechanics.Geometry.Shapes.Tests
+{
+    [TestClass()]
+    public class TestPolygon
+    {
+        private List<Point2D> polyPoints = new List<Point2D>()
+        {
+            new Point2D(0, 0),
+            new Point2D(5, 0),
+            new Point2D(5, 5),
+            new Point2D(0, 0)
+        };
+
+        private List<Point2D> notClosedPolyPoints = new List<Point2D>()
+        {
+            new Point2D(0, 0),
+            new Point2D(5, 0),
+            new Point2D(5, 5)
+        };
+
+        private List<Point2D> centroidPolyPoints = new List<Point2D>()
+        {
+            new Point2D(0, 0),
+            new Point2D(2.5, 0),
+            new Point2D(5, 3)
+        };
+
+        private List<Point2D> closedCentroidPolyPoints = new List<Point2D>()
+        {
+            new Point2D(0, 0),
+            new Point2D(2.5, 0),
+            new Point2D(5, 3)
+        };
+
+        private Polygon shape;
+
+        [TestMethod()]
+        public void TestAreaVerticesClosed()
+        {
+            double expected = 12.5;
+            shape = new Polygon(polyPoints);
+
+            Assert.AreEqual(expected, shape.Area());
+        }
+
+        [TestMethod()]
+        public void TestAreaVerticesNotClosed()
+        {
+            double expected = 12.5;
+            shape = new Polygon(notClosedPolyPoints);
+
+            Assert.AreEqual(expected, shape.Area());
+        }
+
+        [TestMethod()]
+        public void TestCentroidVerticesNotClosed()
+        {
+            Point2D expected = new Point2D(2.5, 1);
+            shape = new Polygon(centroidPolyPoints);
+
+            Assert.AreEqual(expected, shape.Centroid());
+        }
+
+        [TestMethod()]
+        public void TestCentroidVerticesClosed()
+        {
+            Point2D expected = new Point2D(2.5, 1);
+            shape = new Polygon(closedCentroidPolyPoints);
+
+            Assert.AreEqual(expected, shape.Centroid());
+        }
+    }
+}
